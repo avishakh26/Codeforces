801892258bc0e1f3ea24117bbfe5bcfc96f3d335
Avishakh <studiouspook@gmail.com>
2026-03-25 23:38:27 +0600

    basic problem of 800 rating

---
+++ Rating 800/MishkaandGame.py
@@ -0,0 +1,24 @@
+n = int(input())
+
+mishka = 0
+chris = 0
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    
+    
+    
+    if a > b:
+        mishka += 1
+    elif b > a:
+        chris += 1
+
+
+if mishka > chris:
+    print("Mishka")
+    
+elif chris > mishka:
+    print("Chris")
+    
+else:
+    print("Friendship is magic!^^")
