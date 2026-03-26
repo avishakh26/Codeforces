b6ac76f269d3550c3893d60ee5e300be147ddb3a
Avishakh <studiouspook@gmail.com>
2026-03-27 02:23:55 +0600

    solved it too

---
+++ Rating 800/617A_Elephant.py
@@ -0,0 +1,67 @@
+
+import math
+
+a = int(input())
+
+
+
+x = a / 1 
+y = a / 2
+z = a / 3
+xy = a / 4
+xz = a / 5
+
+
+
+mini = min(x,y,z,xy,xz)
+
+
+
+print(math.ceil(mini))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# b,c,d,e,f = (1,2,3,4,5)
+
+# g = True
+
+# while g == True:
+#     xx = a / b 
+    
+#     xy = a / c
+    
+#     xz = a / d
+    
+#     y = a / e
+    
+#     z = a / f
+    
+
+# h = min(xx,xy,xz,y,z)
+
+# print(h)
+
