fa7d7d449707bb2f26ffebe12b3d06389a5b62af
Avishakh <studiouspook@gmail.com>
2026-04-20 15:31:27 +0600

    feat: add character type checker for input validation

---
+++ Assiut University Training - Newcomers/mcaptalorsmall.py
@@ -0,0 +1,12 @@
+X = input().strip()
+
+if X.isdigit():
+    print("IS DIGIT")
+    
+else:
+    print("ALPHA")
+    
+    if X.isupper():
+        print("IS CAPITAL")
+    else:
+        print("IS SMALL")
