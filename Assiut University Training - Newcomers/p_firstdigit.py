aeff077bcf19574a035160e142d43fac351570a1
Avishakh <studiouspook@gmail.com>
2026-05-24 03:37:22 +0600

    night

---
+++ Assiut University Training - Newcomers/p_firstdigit.py
@@ -0,0 +1,18 @@
+X = (input())
+
+first_dig = int(X[0])
+
+if first_dig % 2 == 0:
+    print("EVEN")
+else:
+    print("ODD")
+
+
+# x = input()
+
+# first_digit = int(x[0])
+
+# if first_digit % 2 == 0:
+#     print("EVEN")
+# else:
+#     print("ODD")
